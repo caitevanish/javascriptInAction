bf6fe62a59af8feca49d49897c9f6967012ea6b9
caitevanish <cait.evan@gmail.com>
2022-01-31 09:06:49 -0800

    Added html page. Completed questions 1-3 from Javascript worksheet

---
+++ worksheet.js
@@ -0,0 +1,17 @@
+// //Question 1 -- Variables
+// let dayOfWeek = 'Monday';
+// console.log(dayOfWeek);
+
+// dayOfWeek = 'Friday';
+// console.log('I can\'t wait for ' + dayOfWeek + '!');
+
+
+// // Question 2 -- User Input
+// let animalInput = prompt("What is your favorite animal?");
+// let colorInput = prompt("What is your favorite color?");
+// console.log("I\'ve never seen a " + colorInput + " " + animalInput + "!");
+
+// Question 3 -- Conditionals
+// Favorite breakfast: Avocado Toast
+// Favorite Lunch: Steak salad
+// Favorite Dinner: Vietnamese Pho
