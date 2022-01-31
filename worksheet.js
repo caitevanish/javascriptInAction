0d63b264275ba887cf10cac77e159ebe552e93f6
caitevanish <cait.evan@gmail.com>
2022-01-31 12:43:39 -0800

    Implemented practice on conditionaCompleted Question 3 on the JS worksheet

--- worksheet.js
+++ worksheet.js
@@ -11,7 +11,27 @@
 // let colorInput = prompt("What is your favorite color?");
 // console.log("I\'ve never seen a " + colorInput + " " + animalInput + "!");
 
-// Question 3 -- Conditionals
-// Favorite breakfast: Avocado Toast
-// Favorite Lunch: Steak salad
-// Favorite Dinner: Vietnamese Pho
+// // Question 3 -- Conditionals
+// // Favorite breakfast: Avocado Toast
+// // Favorite Lunch: Steak salad
+// // Favorite Dinner: Vietnamese Pho
+
+
+// let timeOfDay = prompt("What time is it (in military time)?")
+// console.log("It\'s " + timeOfDay + "! You know that that means...")
+// let favoriteMeal = "";
+
+// if(timeOfDay < 1200) {
+//   favoriteMeal = "Avocado Toast";  
+// }
+// else if(timeOfDay > 1200 && timeOfDay < 1700) {
+//   favoriteMeal = "Steak salad";
+// }
+// else if (timeOfDay > 1700) {
+//   favoriteMeal = "Vietnamese Pho";
+// }
+
+// console.log("It\'s " + favoriteMeal + " time!")
+
+
+// Question 4 -- Conditionals
